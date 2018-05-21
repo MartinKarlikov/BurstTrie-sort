a824fd40f3bbd523316025ba3babbf399f389f64
Martin Karlikov <38525726+MartinKarlikov@users.noreply.github.com>
2018-05-21 18:15:49 +0300

    added comments explaining the code

--- BurstTrie.h
+++ BurstTrie.h
@@ -4,23 +4,39 @@
 #include "helperFunctions.h"
 
 #define ALPHABET_SIZE 53
-#define BURST_SIZE 2
+#define BURST_SIZE 128
 
 class BurstTrie
 {
 
 public:
-
+	
+	//default constructor.. demands that the root
+	//will be a type - TrieNode- determining the first letter
 	BurstTrie();
-
+	
+	//calls the desctructor of the root TrieNode which calls
+	//the destructors of the elements in it resulting in
+	//a "recursive" call of all the elemnts of the BurstTrie
 	~BurstTrie();
 
+	//the add function
+	//continues on the length of the word
+	//skips down on the TrieNode and adds the suffix in the desired bucket
+	//or at some TrieNode if the word is a prefix of another already contained word
 	void add(string);
 
-	bool contains(string);
+	//result- if the given word is contained
+	//in the BurstTrie
+	bool contains(string) const;
 
+	//result- gives us the count of the words contained
+	//in the BurstTrie
 	size_t wordCount()const;
 
+	//the actual sorting algorithm which returns the sorted set of words
+	//in any desired container given to the function
+	//works fastest with a list
 	template<typename T>
 	T sortedConvert() const;
 
